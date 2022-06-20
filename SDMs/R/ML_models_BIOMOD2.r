faf9499c64458815d7a91d7fc7a0b7ea2b825cb4
Khondamir Rustamov <69417652+KhondamirRustamov@users.noreply.github.com>
2022-06-20 11:46:46 +0500

    Update ML_models_BIOMOD2.r

--- SDMs/R/ML_models_BIOMOD2.r
+++ SDMs/R/ML_models_BIOMOD2.r
@@ -5,20 +5,23 @@ library(rgbif) # One of many GBIF access points (Global Biodiversity Information
 library(maps) # Easy access to basic map layers
 library(biomod2) # Ensemble SDM package
 
-gbif.PIPO.OR.absent <- read.table('D:/DIVA-GIS/climate/presence/pseudo_affinis_rsep.csv', header=TRUE, sep=',')
+
+# download pseudo-absence data
+gbif.PIPO.OR.absent <- read.table('data/pseudo_absence_rsep.csv', header=TRUE, sep=',')
 colnames(gbif.PIPO.OR.absent) <- c('name', 'lon', 'lat')
 coordinates(gbif.PIPO.OR.absent) <- ~ lon + lat
-
 gbif.PIPO.OR.absent <- SpatialPointsDataFrame(coords = coordinates(gbif.PIPO.OR.absent),
                                               data = data.frame(gbif.PIPO.OR.absent))
 
-gbif.PIPO.OR.present <- read.table('D:/DIVA-GIS/climate/presence/Tulipa_affinis.csv', header=TRUE, sep=',')
+
+# download present data
+gbif.PIPO.OR.present <- read.table('data/present.csv', header=TRUE, sep=',')
 colnames(gbif.PIPO.OR.present) <- c('name', 'lon', 'lat' )
 coordinates(gbif.PIPO.OR.present) <- ~ lon + lat
-
 gbif.PIPO.OR.present <- SpatialPointsDataFrame(coords = coordinates(gbif.PIPO.OR.present),
                                        data = data.frame(gbif.PIPO.OR.present))
 
+# visualizing present and pa points on the Uzbekistan map
 map('world', xlim = c(55, 75), ylim=c(36,47))
 points(gbif.PIPO.OR.absent, pch=21, bg='dodgerblue')
 points(gbif.PIPO.OR.present, pch=21, bg='red')
@@ -28,9 +31,12 @@ gbif.PIPO.OR.absent@data <- data.frame(present = rep(0, nrow(gbif.PIPO.OR.absent
 
 PIPO.dat <- rbind(gbif.PIPO.OR.present, gbif.PIPO.OR.absent)
 
+
+# dowbload worldclim data
 files <- list.files(path='D:/DIVA-GIS/wc2.1_30s_bio', pattern='tif', full.names=TRUE)
 predictors <- stack(files)
 
+# crop worldclim data
 ext <- extent(65, 75, 36, 44)
 predictors <- crop(predictors, ext)
 predictors[is.nan(predictors)] <- 0
@@ -40,7 +46,8 @@ env.dat <- extract(x = predictors, y=PIPO.dat)
 PIPO.mod.dat <- BIOMOD_FormatingData(resp.var = PIPO.dat,
                                      expl.var = stack(predictors),
                                      resp.name = 'Tulipa affinis')
-                                           
+
+# modeling
 PIPO.mod <- BIOMOD_Modeling(data = PIPO.mod.dat,
                             models = c('GLM', 'GBM', 'RF', 'MARS', 'CTA', 'SRE', 'FDA', 'MAXENT.Phillips'),
                             SaveObj = TRUE,
@@ -48,10 +55,11 @@ PIPO.mod <- BIOMOD_Modeling(data = PIPO.mod.dat,
                             DataSplit=80,
                             VarImport=0)
 
+# SDM performance evaluating
 PIPO.mod.eval <- get_evaluations(PIPO.mod)
 print(PIPO.mod.eval)
 
-
+# create prediction maps using all models and save in .grd file
 myBiomodProj <- BIOMOD_Projection(modeling.output = PIPO.mod,
                                   new.env = stack(predictors), # modern environment
                                   proj.name = 'current',
@@ -62,5 +70,6 @@ myBiomodProj <- BIOMOD_Projection(modeling.output = PIPO.mod,
                                   do.stack = F,
                                   output.format = '.grd')
 
+# save predictions
 write.table(get_predictions(PIPO.mod), file='D:/DIVA-GIS/ML prediction/Tulipa_affinis/test1_af.csv', row.names=FALSE)
 
